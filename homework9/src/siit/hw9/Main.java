02477cc4a00f878d7e3c959e747e00719081c018
puftu <Pluch1991>
2019-09-19 22:40:00 +0300

    Javadoc added in main

--- homework9/src/siit/hw9/Main.java
+++ homework9/src/siit/hw9/Main.java
@@ -4,9 +4,11 @@ package siit.hw9;
 public class Main {
 
     public static void main(String[] args) {
-
+        //Path of the input file
         String inputPath = "E:\\SIIT\\scoala-informala-homework\\homework9\\src\\siit\\hw9\\input.txt";
+        //Path of the output file
         String outputPath = "E:\\SIIT\\scoala-informala-homework\\homework9\\src\\siit\\hw9\\output.txt";
+        // Target month/ Can be changed.
         int targetMonth = 12;
 
         App.getPersonByMonth(inputPath, targetMonth, outputPath);
